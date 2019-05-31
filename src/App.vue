09ac838d449a07ba921f9201a9f87a75f059af5f
Toni <toni_lam@hotmail.com>
2019-05-31 11:16:49 +1000

    Install bootstrap and font-awesome

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,34 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sortable-button field="test" v-model="order"></sortable-button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SortableButton from "./components/SortableButton";
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    SortableButton,
+  },
+  data() {
+    return {
+      order: {
+        field: null,
+        order: null,
+      },
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '~bootstrap/scss/bootstrap';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
+
+/* Font Awesome config */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   render: h => h(App),
